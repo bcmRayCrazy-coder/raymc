886d1fe8024f88089a1fa35485cc8698cc35b35a
bcmray <975875040@qq.com>
2026-06-19 00:18:38 +0800

    Fix audio play error

--- src/audio/track.rs
+++ src/audio/track.rs
@@ -13,8 +13,9 @@ pub struct AudioTrack {
     sample: Vec<[f32; 2]>,
     sample_rate: u32,
     is_playing: bool,
+    frac:f64,
     pub stop_pos: usize,
-    pub current_pos: f32,
+    pub current_pos: usize,
     pub volume: f32,
 }
 
@@ -22,8 +23,9 @@ impl AudioTrack {
     pub fn new(track_type: AudioTrackType, sample: Vec<[f32; 2]>, sample_rate: u32) -> Self {
         Self {
             stop_pos: sample.len(),
-            current_pos: 0.0,
+            current_pos: 0,
             volume: 1.0,
+            frac:0.0,
 
             track_type,
             sample,
@@ -40,10 +42,25 @@ impl AudioTrack {
             self.set_playing(false);
             return ([0.0; 2], true);
         }
-        let pos = self.current_pos.floor() as usize;
-        let sample = self.sample[pos].map(|s| s * self.volume);
-        self.current_pos += self.sample_rate as f32 / target_sample_rate as f32;
-        return (sample, false);
+        
+        let next_pos = self.current_pos + 1;
+        let next = if next_pos >= self.sample.len() {
+            [0.0, 0.0]
+        } else {
+            self.sample[next_pos]
+        };
+
+        let mut sample = self.sample[self.current_pos];
+        sample[0] = sample[0] * self.frac as f32 + next[0] - next[0] * self.frac as f32;
+        sample[1] = sample[1] * self.frac as f32 + next[1] - next[1] * self.frac as f32;
+        let step = self.sample_rate as f64 / target_sample_rate as f64;
+        self.frac += step;
+        if self.frac >= 1.0 {
+            let floor = self.frac.floor();
+            self.current_pos += floor as usize;
+            self.frac -= floor;
+        }
+        return (sample.map(|s| s * self.volume), false);
     }
 
     pub fn track_type(&self) -> AudioTrackType {
@@ -59,7 +76,7 @@ impl AudioTrack {
     }
 
     pub fn is_end(&self) -> bool {
-        self.current_pos >= self.stop_pos as f32
+        self.current_pos >= self.stop_pos
     }
 
     pub fn set_playing(&mut self, play: bool) {
@@ -67,7 +84,7 @@ impl AudioTrack {
     }
 
     pub fn replay(&mut self) {
-        self.current_pos = 0.0;
+        self.current_pos = 0;
         self.is_playing = true;
     }
 }
